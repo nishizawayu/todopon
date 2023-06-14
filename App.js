d3b23d719ca50a2848b43a9c66b1b58c02f369f9
nishizawayuya <22aw0128@jec.ac.jp>
2023-06-14 17:41:40 +0900

    navの修正

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { ImageComponent, StyleSheet, Text, View } from 'react-native';
 import Todo from './components/Todo';
 import Calendars from './components/Calender';
+import Nav from './components/Nav';
 
 export default function App() {
   return (
-    <Calendars/>
+    <Nav/>
   );
 }
 
--- components/Todo.js
+++ components/Todo.js
@@ -3,74 +3,8 @@ import { StatusBar } from 'expo-status-bar';
 import { Keyboard, Platform, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View, Image } from 'react-native';
 import Task from './Task';
 import { KeyboardAvoidingView } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
-
-const Tab = createMaterialBottomTabNavigator();
-
-export default function Todo() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-        activeColor="#FFAF37"
-        inactiveColor="#000"
-        labelStyle={{ fontSize: 12,}}
-        barStyle={{ backgroundColor: '#fff',height:100}}
-      >
-        <Tab.Screen
-        name="SettingsScreen"
-        component={SettingsScreen}
-        options={{
-          tabBarLabel: '図鑑',
-          tabBarIcon: () => (
-            <Image 
-                source={require('../assets/img/gallery.png')}
-                style={{width:35,height:30}}
-            />
-          ),
-        }}
-      />
-        <Tab.Screen 
-            name="Todo" 
-            component={TodoList} 
-            options={{
-                tabBarLabel: 'Todo',
-                tabBarIcon:() => (
-                  <Image 
-                    source={require('../assets/img/Todo.png')}
-                    style={{width:35,height:30}}
-                  />
-                ),
-              }}
-        />
-        <Tab.Screen 
-            name="ガチャ" 
-            component={SettingsScreen}
-            options={{
-                tabBarLabel: 'ガチャ',
-                tabBarIcon:() => (
-                  <Image 
-                    source={require('../assets/img/gatya.png')}
-                    style={{width:35,height:30}}
-                  />
-                ),
-              }} 
-        
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-}
-
-function TodoList() {
+ 
+export default function TodoList() {
 
   const [task,setTask]= useState("");
   const [taskItems,settaskItems] = useState([]);
