5e0df1893f8204e878080501b0c918d66de6db33
yuuki0719 <yuuki20030719@gmail.com>
2023-06-20 10:40:30 +0900

    firstcomit

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import TodoList from './components/Todo';
+import Top from './components/Top';
+import Gatya from './components/Gatya';
 
 export default function App() {
   return (
     <>
+    <Gatya />
       <View style={styles.container}>
         <Text>かす</Text>
       </View>
