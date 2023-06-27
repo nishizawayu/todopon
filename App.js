47d5c3ea0211d3a17735f463e12fbebf93bd393b
yuuki0719 <yuuki20030719@gmail.com>
2023-06-27 11:21:27 +0900

    スプラッシュ画面

--- App.js
+++ App.js
@@ -1,22 +1,14 @@
-import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-
-import { ImageComponent, StyleSheet, Text, View } from 'react-native';
-import Todo from './components/Todo';
-import Calendars from './components/Calender';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import * as SplashScreen from 'expo-splash-screen';
 import Nav from './components/Nav';
 
-export default function App() {
-  return (
-    <Nav/>
-  );
-}
+export default function App(){
+SplashScreen.preventAutoHideAsync();
+setTimeout(SplashScreen.hideAsync, 2000);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+return(
+  <SafeAreaProvider>
+    <Nav />
+  </SafeAreaProvider>
+)
+}
--- components/Top.js
+++ components/Top.js
@@ -5,27 +5,7 @@ export default function Top() {
 
     return (
         <>
-        <Image
-          style={{
-            width: 370,
-            height: 179
-          }}
-          source={require('../assets/img/logo.png')}
-        />
-        <Image
-          style={{
-            width: 250,
-            height: 380
-          }}
-          source={require('../assets/img/gatya_top.png')}
-        />
-                <Image
-          style={{
-            width: 390,
-            height: 231
-          }}
-          source={require('../assets/img/table.jpg')}
-        />
+        
         </>
     );
   }
