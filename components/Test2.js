1c7bd8c705cc80348b328004d3914b61f249b243
nishizawayuya <22aw0128@jec.ac.jp>
2023-07-28 01:03:05 +0900

    ギャラリーの見た目

--- components/Test2.js
+++ components/Test2.js
@@ -22,6 +22,8 @@ function GalleryHome({navigation}){
       }}>図鑑</Text>
       <View>
         <View style={{flexDirection:'row',flexWrap:"wrap",marginTop:100}}>
+
+          {/* ケーキ */}
           <TouchableOpacity style={styles.galleryitem} onPress={()=> navigation.navigate('Sweets')}>
             <Image 
               source={require("../assets/img/ribbon.png")}
@@ -36,27 +38,31 @@ function GalleryHome({navigation}){
               夜のお菓子屋さん
             </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.galleryitem}onPress={()=> navigation.navigate('Sweets')}>
+
+          {/* ゲーム */}
+          <TouchableOpacity style={styles.galleryitem}onPress={()=> navigation.navigate('Game')}>
             <Image 
               source={require("../assets/img/ribbon.png")}
               style={{marginLeft:10}}
             />
             <Image 
-              source={require("../assets/img/gatyamachine.png")}
+              source={require("../assets/img/game_gatya.png")}
               style={{width:135,height:178,marginLeft:8}}
             />
             <View style={{borderBottomWidth:1,borderColor:"#AEACAC"}}></View>
             <Text style={styles.gallerytext}>
-              夜のお菓子屋さん
+              CRおじじ
             </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.galleryitem}onPress={()=> navigation.navigate('Sweets')}>
+
+          {/* 鶏 */}
+          <TouchableOpacity style={styles.galleryitem}onPress={()=> navigation.navigate('Egg')}>
             <Image 
               source={require("../assets/img/ribbon.png")}
               style={{marginLeft:10}}
             />
             <Image 
-              source={require("../assets/img/gatyamachine.png")}
+              source={require("../assets/img/chicken_gatya.png")}
               style={{width:135,height:178,marginLeft:8}}
             />
             <View style={{borderBottomWidth:1,borderColor:"#AEACAC"}}></View>
@@ -64,13 +70,15 @@ function GalleryHome({navigation}){
               鶏の卵
             </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.galleryitem}onPress={()=> navigation.navigate('Sweets')}>
+
+          {/* ぶた */}
+          <TouchableOpacity style={styles.galleryitem}onPress={()=> navigation.navigate('Pig')}>
             <Image 
               source={require("../assets/img/ribbon.png")}
               style={{marginLeft:10}}
             />
             <Image 
-              source={require("../assets/img/gatyamachine.png")}
+              source={require("../assets/img/pig_gatya.png")}
               style={{width:135,height:178,marginLeft:8}}
             />
             <View style={{borderBottomWidth:1,borderColor:"#AEACAC"}}></View>
@@ -78,6 +86,7 @@ function GalleryHome({navigation}){
               豚の食べ方
             </Text>
           </TouchableOpacity>
+
         </View>
       </View>
     </View>
@@ -116,6 +125,100 @@ function Sweets({navigation}){
   )
 }
 
+function Egg({navigation}){
+  return(
+    <View style={styles.container}>
+      <ImageBackground source={require("../assets/img/Egg_bg.png")} style={{flex:1,justifyContent:"center"}}>
+        <TouchableOpacity style={{position:'absolute',top:84,left:30,zIndex:2,padding:10,paddingRight:20}} onPress={()=> navigation.navigate('Home')}>
+          <Image 
+            source={require("../assets/img/pageback.png")}
+          />
+        </TouchableOpacity>
+        <View style={{
+          position:"absolute",
+          top:80,
+          borderColor:"#FFAB73",
+          borderWidth:3,
+          backgroundColor:"#fff",
+          width:"90%",
+          marginLeft:"5%",
+        }}>
+          <Text style={{
+            fontSize:24,
+            textAlign:"center",
+            paddingTop:10,
+            paddingBottom:10,
+          }}>
+            鶏の卵
+          </Text>
+        </View>
+      </ImageBackground>
+    </View>
+  )
+}
+function Pig({navigation}){
+  return(
+    <View style={styles.container}>
+      <ImageBackground source={require("../assets/img/Pig_bg.png")} style={{flex:1,justifyContent:"center"}}>
+        <TouchableOpacity style={{position:'absolute',top:84,left:30,zIndex:2,padding:10,paddingRight:20}} onPress={()=> navigation.navigate('Home')}>
+          <Image 
+            source={require("../assets/img/pageback.png")}
+          />
+        </TouchableOpacity>
+        <View style={{
+          position:"absolute",
+          top:80,
+          borderColor:"#FFAB73",
+          borderWidth:3,
+          backgroundColor:"#fff",
+          width:"90%",
+          marginLeft:"5%",
+        }}>
+          <Text style={{
+            fontSize:24,
+            textAlign:"center",
+            paddingTop:10,
+            paddingBottom:10,
+          }}>
+            豚の食べ方
+          </Text>
+        </View>
+      </ImageBackground>
+    </View>
+  )
+}
+function Game({navigation}){
+  return(
+    <View style={styles.container}>
+      <ImageBackground source={require("../assets/img/Game_bg.png")} style={{flex:1,justifyContent:"center"}}>
+        <TouchableOpacity style={{position:'absolute',top:84,left:30,zIndex:2,padding:10,paddingRight:20}} onPress={()=> navigation.navigate('Home')}>
+          <Image 
+            source={require("../assets/img/pageback.png")}
+          />
+        </TouchableOpacity>
+        <View style={{
+          position:"absolute",
+          top:80,
+          borderColor:"#FFAB73",
+          borderWidth:3,
+          backgroundColor:"#fff",
+          width:"90%",
+          marginLeft:"5%",
+        }}>
+          <Text style={{
+            fontSize:24,
+            textAlign:"center",
+            paddingTop:10,
+            paddingBottom:10,
+          }}>
+            CR
+          </Text>
+        </View>
+      </ImageBackground>
+    </View>
+  )
+}
+
 
 const Stack = createNativeStackNavigator();
 
@@ -127,6 +230,9 @@ function Test2() {
       }}>
         <Stack.Screen name="Home" component={GalleryHome} />
         <Stack.Screen name="Sweets" component={Sweets} />
+        <Stack.Screen name="Egg" component={Egg} />
+        <Stack.Screen name="Pig" component={Pig} />
+        <Stack.Screen name="Game" component={Game} />
       </Stack.Navigator>
   );
 }
